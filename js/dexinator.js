6e969948cf4b88bebc04a4e75d7f7bd1005874a5
JD Bothma <jbothma@gmail.com>
2016-06-17 11:43:24 +0200

    Add dexinate events

--- js/dexinator.js
+++ js/dexinator.js
@@ -1,16 +1,15 @@
 function dexinate(event) {
     var input = $(event.target).val();
 
+    ga('send', 'event', 'dexinate-input', input);
+
     input = input.replace("<br>", "<br />");
     input = input.replace("></script>", "> </script>");
     input = input.replace("></iframe>", "> </iframe>");
 
-
     $("textarea[name='output']").val(input);
 
-    $("#preview").empty();
-    $("#preview").append($(input));
-
+    ga('send', 'event', 'dexinate-result', input);
 }
 
 var inputEl = $("textarea[name='input']");
